6cc26b0159fdef2dce5f8d2ffeb22ec29c63e12b
NonAtomicFunk <a.a.berezovsky@gmail.com>
2023-08-08 16:50:54 +0300

    Initial Commit

---
+++ NewsListApp/NewsListAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  NewsListAppApp.swift
+//  NewsListApp
+//
+//  Created by Alexander Berezovsky on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsListAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
